71775d4752a0bc1b849a8daa6d48a58d4a162d1a
Anders Dahnielson <anders@dahnielson.com>
2014-07-15 14:59:51 +0200

    Trying to improve the PDF output

--- conf.py
+++ conf.py
@@ -199,7 +199,7 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = False
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
